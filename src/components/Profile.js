aee6c0a040c58c5fc4e7416ac90a45b7fae7e0fe
tuturu-tech <elvis.skozdopolj@gmail.com>
2021-11-20 00:32:40 +0100

    Basic user updating

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,11 +4,69 @@ import { getEllipsisTxt } from '../helpers/formatters';
 import Blockie from './Blockie';
 import { useState } from 'react';
 import Address from './Address/Address';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { Grid } from '@mui/material';
+import { TextField } from '@mui/material';
+import Button from '@mui/material/Button';
 
 const Profile = () => {
-  const { setUserData, userError, isUserUpdating, user } = useMoralis();
+  const { setUserData, userError, isUserUpdating, user, isAuthUndefined } = useMoralis();
+  const [username, setUsername] = useState('');
+  const [bio, setBio] = useState('');
 
-  return <div>Nothing here for now</div>;
+  if (isAuthUndefined) {
+    return <div>loading</div>;
+  }
+
+  const handleClick = () => {
+    setUserData({
+      username: username,
+      bio: bio,
+    });
+  };
+
+  return (
+    <div>
+      <Box>
+        <Blockie currentWallet scale={20} />
+        <Grid>
+          <Typography style={{ border: '1px' }} mt={2}>
+            {user.attributes.username}
+          </Typography>
+          <Address size={14} copyable style={{ fontSize: '20px', justifyContent: 'center' }} />
+          <Typography mt={2}> {user.attributes.bio ? user.attributes.bio : 'Empty bio'} </Typography>
+        </Grid>
+        <Box style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            value={username}
+            margin="normal"
+            onChange={(event) => {
+              console.log(event.target.value);
+              setUsername(event.target.value);
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Bio"
+            variant="outlined"
+            margin="normal"
+            value={bio}
+            onChange={(event) => {
+              console.log(event.target.value);
+              setBio(event.target.value);
+            }}
+          />
+          <Button onClick={handleClick} disabled={isUserUpdating}>
+            Submit changes
+          </Button>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Profile;
